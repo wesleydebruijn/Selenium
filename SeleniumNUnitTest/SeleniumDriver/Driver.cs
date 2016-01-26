fb372b70ff18ebb4b6881548e68ea904d716f92c
Wesley de Bruijn <wesley.dbr@live.nl>
2016-01-26 11:50:50 +0100

    Google demo added

---
+++ SeleniumNUnitTest/SeleniumDriver/Driver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumDriver
+{
+    public class Driver
+    {
+        public static IWebDriver driver;
+
+        public static void Main(String[] args)
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.nl");
+            driver.Manage().Window.Maximize();
+
+            // DOM Manipulatie voor tweede demo
+            //DOMManipulation();
+
+            // Events voor derde demo
+            //Events();  
+
+            // driver.Close();
+        }
+
+
+        private static void DOMManipulation()
+        {
+            // Zoekveld van Google
+            IWebElement searchbar = driver.FindElement(By.Id("lst-ib"));
+            searchbar.SendKeys("Selenium framework");
+        }
+
+        private static void Events()
+        {
+            // Zoekbutton van Google
+            IWebElement button = driver.FindElement(By.Name("btnG"));
+            button.Click();
+        }
+    }
+}
